324e7f139e2bba3e63153e22eb8c6c72e679f074
Mohamed Amoussa <126504124+kephrenh@users.noreply.github.com>
2023-11-01 16:40:30 +0100

    Intégration js des boutons de filtre

--- FrontEnd/assets/scripts/works.js
+++ FrontEnd/assets/scripts/works.js
@@ -35,3 +35,153 @@ function getWorks(works) {
         gallery.appendChild(figure);
     })
 }
+
+// Création de la div contenant les boutons
+function createFiltersDiv() {
+    let filters = document.createElement("div");
+    filters.classList.add("filters");
+
+    portfolio.insertBefore(filters, gallery);
+}
+createFiltersDiv();
+
+// Création et ajout des boutons
+function createFiltersBtns() {
+
+    for (let i = 0; i < 4; i++) {
+        let filters = document.querySelector(".filters");
+
+        const filterBtn = document.createElement("button");
+        filterBtn.classList.add("btn");
+
+        const lineBreak = document.createElement("br");
+
+        filters.appendChild(filterBtn);
+        filters.appendChild(lineBreak)
+    }
+
+}
+createFiltersBtns();
+
+// Ajout de texte aux boutons
+const listFilters = document.querySelectorAll(".btn");
+
+function addFilterBtnTxt() {
+
+    listFilters[0].textContent = "Tous";
+    listFilters[1].textContent = "Objets";
+    listFilters[2].textContent = "Appartements";
+    listFilters[3].textContent = "Hôtels & restaurants";
+
+}
+addFilterBtnTxt();
+
+// Ajout de classe aux boutons
+function addFilterBtnClass() {
+    listFilters[0].classList.add("btn-all");
+    listFilters[1].classList.add("btn-items");
+    listFilters[2].classList.add("btn-apartments");
+    listFilters[3].classList.add("btn-hotels");
+}
+addFilterBtnClass();
+
+// Ajout filtre objets
+function filteredItems() {
+    const works = document.querySelectorAll(".project");
+
+    works.forEach((work) => {
+        const categoryId = work.getAttribute("category-id");
+        if (categoryId === "1") {
+            work.style.display = "block";
+        } else {
+            work.style.display = "none";
+        }
+    });
+}
+const btnItems = document.querySelector(".btn-items");
+btnItems.addEventListener("click", filteredItems);
+
+// Ajout filtre appartements
+function filteredApartments() {
+    const works = document.querySelectorAll(".project");
+
+    works.forEach((work) => {
+        const categoryId = work.getAttribute("category-id");
+        if (categoryId === "2") {
+            work.style.display = "block";
+        } else {
+            work.style.display = "none";
+        }
+    });
+}
+const btnApartments = document.querySelector(".btn-apartments");
+btnApartments.addEventListener("click", filteredApartments);
+
+// Ajout filtre hôtels & restaurants
+function filteredHotels() {
+    const works = document.querySelectorAll(".project");
+
+    works.forEach((work) => {
+        const categoryId = work.getAttribute("category-id");
+        if (categoryId === "3") {
+            work.style.display = "block";
+        } else {
+            work.style.display = "none";
+        }
+    });
+}
+const btnHotels = document.querySelector(".btn-hotels");
+btnHotels.addEventListener("click", filteredHotels);
+
+// Ajout filre Tous
+function noFilter() {
+    const works = document.querySelectorAll(".project");
+    works.forEach((work) => {
+        work.style.display = "block";
+    });
+}
+const btnAll = document.querySelector(".btn-all");
+btnAll.addEventListener("click", noFilter);
+
+
+// Changement aspect des boutons filtre au click
+const filterBtns = document.querySelectorAll(".filters .btn");
+const nbFilterBtn = filterBtns.length;
+
+const filterBtn = filterBtns[i];
+
+// Filtre Tous sélectionné par défaut au chargement de la page
+function defaultFilter() {
+    filterBtns[0].classList.add("btn_selected");
+}
+
+window.addEventListener("load", noFilter);
+window.addEventListener("load", defaultFilter);
+
+//Modification style des filtres après sélection
+filterBtns.forEach((filterBtn) => {
+
+    filterBtn.addEventListener("click", () => {
+        console.log("clicked");
+
+        filterBtns[0].classList.remove("btn_selected");
+        if (i === 1) {
+            filterBtn.classList.add("btn_selected");
+        }
+
+        filterBtns[1].classList.remove("btn_selected");
+        if (i === 2) {
+            filterBtn.classList.add("btn_selected");
+        }
+
+        filterBtns[2].classList.remove("btn_selected");
+        if (i === 3) {
+            filterBtn.classList.add("btn_selected");
+        }
+
+        filterBtns[3].classList.remove("btn_selected");
+        if (i === 0) {
+            filterBtn.classList.add("btn_selected");
+        }
+    })
+})
