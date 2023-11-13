0e3bc857043ab1cfd566ecb21cb4a060087747de
Mohamed Amoussa <mohamedj81@hotmail.com>
2023-11-13 15:08:44 +0100

    Ajouter popup message d'erreur

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -5,9 +5,9 @@ const form = {
     password : document.getElementById("password")
 }
 
-const errorMessage = document.querySelector(".errorMessage");
+const main = document.querySelector("main");
+const loginForm = document.getElementById("loginForm");
 const submitUser = document.getElementById("submit-user");
-
 
 const login = (e)=> {
     e.preventDefault();
@@ -27,17 +27,55 @@ const login = (e)=> {
         body: payload,
     })
     .then(response => {
-        if(response.ok) {
-            return response.json();
-        } else if(response.status === 404 || response.status === 401) {
-            throw new Error("Données d'identifications incorrectes");
-        }
+        if(response.ok) return response.json();
+        if(response.status === 404 || response.status === 401) return openDialog();
     })
     .then(data => {
+        sessionStorage.setItem("isLogged", JSON.stringify(true));
         sessionStorage.setItem("token", data.token);
         window.location.replace("index.html")
-        console.log(data)
     })
 }
 submitUser.addEventListener("click", login);    
         
+function createErrorDialog() {
+    const dialog = document.createElement("dialog");
+    dialog.classList.add("errorDialog");
+    
+    const dialogTitle = document.createElement("h2");
+    dialogTitle.classList.add("dialogTitle");
+    dialogTitle.innerHTML = "Login Failed";
+
+    const dialogText = document.createElement("p");
+    dialogText.classList.add("dialogText");
+    dialogText.innerHTML = "Invalid email address or password";
+
+    const dialogButton = document.createElement("i");
+    dialogButton.classList.add("dialogButton");
+    dialogButton.classList.add("fa-regular");
+    dialogButton.classList.add("fa-circle-xmark");
+
+    dialog.appendChild(dialogTitle);
+    dialog.appendChild(dialogText);
+    dialog.appendChild(dialogButton);
+
+    main.appendChild(dialog);
+}
+createErrorDialog();
+
+function openDialog() {
+    document.querySelector("body").style.overflow = "hidden";
+    const errorDialog = document.querySelector(".errorDialog");
+    errorDialog.style.display = "flex";
+    errorDialog.showModal();
+}
+
+const closeDialog = (e)=> {
+    e.preventDefault();
+    document.querySelector("body").style.overflow = "visible";
+    const errorDialog = document.querySelector(".errorDialog");
+    errorDialog.style.display = "none";
+    errorDialog.close()
+}
+const closeButton = document.querySelector(".dialogButton");
+closeButton.addEventListener("click", closeDialog);
